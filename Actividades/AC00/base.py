a9369cee3942d8efa28ff6689a745fdc5996063d
Tomas Rivera <tvrivera@uc.cl>
2019-08-08 16:25:01 -0400

    bienvendia y area cuadrado ok

---
+++ Actividades/AC00/base.py
@@ -0,0 +1,26 @@
+# Seccion para importar liberías
+
+
+
+class Cuadrado(object):
+	def __init__(self, lado):
+		self.lado = int(lado)
+
+	def area(self):
+		return self.lado**2
+
+
+class Triangulo(object):
+	def __init__(self, arg):
+		# Completar método y borrar pass
+		pass		
+	# Agregar métodos
+		
+		
+if __name__ == '__main__':
+	a = Cuadrado(2)
+	print(a.area())
+
+
+
+
