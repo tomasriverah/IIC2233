3df08d442ad0501e0ac76715ee7bfe7c18000217
Tomas Rivera <tvrivera@uc.cl>
2019-09-15 21:09:15 -0300

    menus agregados muy tarde

--- Tareas/T01/Menus.py
+++ Tareas/T01/Menus.py
@@ -1,14 +1,15 @@
+import parametros
 
 
 class Menu:
     def __init__(self):
         self.opciones = []
         self.lineas = []
+        self.eleccion = -1
 
     def mostrar_menu(self):
         for linea in self.lineas:
             print(linea)
-
 
     def recibir_input(self):
         boleano = True
@@ -16,8 +17,7 @@ class Menu:
             eleccion = input("Ingrese un número válido:")
             if eleccion in self.opciones:
                 boleano = False
-        return eleccion
-
+        self.eleccion = eleccion
 
 
 class MenuSesion(Menu):
@@ -25,13 +25,7 @@ class MenuSesion(Menu):
     def __init__(self,):
             Menu.__init__(self)
             self.opciones = ["1", "2", "0"]
-            self.lineas = ["[1] Nueva Partida", "[2] Cargar Partida, [0] Salir"]
-        
-    def nueva_partida(self):
-        pass
-
-    def cargar_partida(self):
-        pass
+            self.lineas = ["[1] Nueva Partida", "[2] Cargar Partida","[0] Salir"]
 
 
 class MenuPrincipal(Menu):
@@ -41,27 +35,57 @@ class MenuPrincipal(Menu):
         self.lineas = ["[1] Comprar nuevos vehiculos", "[2] Nueva Carrera", "[3] Guardar Partida"
                        , "[0] Salir"]
 
-class MenuCompra(Menu):
-    pass
 
 class MenuPreparacion(Menu):
-    pass
+    def __init__(self):
+        Menu.__init__(self)
+        self.opciones = ["1", "0"]
+        self.lineas = ["[1] Seleccionar pista y vehículo", "[0] Salir"]
+
 
 class MenuCarrera(Menu):
-    pass
+    def __init__(self):
+        Menu.__init__(self)
+        self.opciones = ["1", "0"]
+        self.lineas = ["[1] Entrar a pits", "[0] Seguir en pista"]
+
 
 class MenuPits(Menu):
-    pass
+    def __init__(self, piloto):
+        Menu.__init__(self)
+        self.piloto= piloto
+        self.mejoras = parametros.MEJORAS
+        self.opciones = ["1", "2", "3", "4","0"]
+        self.lineas = [f"Dinero disponible ${self.piloto.dinero}",
+                       f"[1] CHASIS ${self.mejoras['CHASIS']['COSTO']}",
+                       f"[2] CARROCERIA ${self.mejoras['CARROCERIA']['COSTO']}",
+                       f"[3] RUEDAS ${self.mejoras['RUEDAS']['COSTO']}",
+                       f"[4] MOTOR/ZAPATILLAS ${self.mejoras['MOTOR/ZAPATILLAS']['COSTO']}",
+                       "[0] Volver a pista sin mejora" ]
+        self.dicc = {"1":"CHASIS", "2":"CARROCERIA", "3":"RUEDAS", "4":"MOTOR/ZAPATILLAS"}
+
+
+    def aplica_mejoras(self):
+        parte = self.dicc[self.eleccion]
+        if (self.mejoras[parte]["COSTO"]) > self.piloto.dinero:
+            print("***Dinero insfuciente***")
+            print("***Por no prestar atención vuelves a pista***")
+        opcion = str.lower(parte)
+        if parte == "MOTOR/ZAPATILLAS":
+            opcion = "motor"
+        else:
+            self.piloto.dinero -= int(self.mejoras[parte]["COSTO"])
+            multiplier = self.mejoras[parte]["EFECTO"]
+            setattr(self.piloto.vehiculo, opcion, getattr(self.piloto.vehiculo, opcion)
+                    * multiplier)
+            return self.piloto.dinero
 
 
 
 
 
 
-hola = MenuSesion()
-holi = MenuPrincipal()
-holi.mostrar_menu()
-holi.recibir_input()
+
 
 
 
